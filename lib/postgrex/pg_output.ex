3129028fbecde3c41d4ca9bf06d0bd7908ba5518
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2022-11-08 13:46:29 +0100

    fix docs indent

--- lib/postgrex/pg_output.ex
+++ lib/postgrex/pg_output.ex
@@ -8,19 +8,20 @@ defmodule Postgrex.PgOutput do
 
   * Primary keep alive
 
-      import Postgrex.PgOutput.Messages
+        import Postgrex.PgOutput.Messages
 
-      msg = <<107, 0, 0, 0, 0, 1, 130, 140, 128, 0, 2, 141, 89, 193, 229, 5, 73, 1>>
-      msg_primary_keep_alive(reply: 1) = pka = Postgrex.PgOutput.decode(msg)
+        msg = <<107, 0, 0, 0, 0, 1, 130, 140, 128, 0, 2, 141, 89, 193, 229, 5, 73, 1>>
+        msg_primary_keep_alive(reply: 1) = pka = Postgrex.PgOutput.decode(msg)
 
   * Xlog data
-      import Postgrex.PgOutput.Messages
 
-      msg = <<119, 0, 0, 0, 0, 1, 94, 109, 184, 0, 0, 0, 0, 1, 94, 109, 184, 0, 2, 141, 89, 243, 12,
-        106, 120, 66, 0, 0, 0, 0, 40, 218, 86, 184, 0, 2, 116, 137, 36, 88, 241, 171, 0, 6, 166,
-        173>>
+        import Postgrex.PgOutput.Messages
 
-      msg_xlog_data(data: msg_begin() = begin) = Postgrex.PgOutput.decode(msg)
+        msg = <<119, 0, 0, 0, 0, 1, 94, 109, 184, 0, 0, 0, 0, 1, 94, 109, 184, 0, 2, 141, 89, 243, 12,
+          106, 120, 66, 0, 0, 0, 0, 40, 218, 86, 184, 0, 2, 116, 137, 36, 88, 241, 171, 0, 6, 166,
+          173>>
+
+        msg_xlog_data(data: msg_begin() = begin) = Postgrex.PgOutput.decode(msg)
   """
   @spec decode(binary()) :: term()
   defdelegate decode(msg), to: __MODULE__.Messages
@@ -52,19 +53,19 @@ defmodule Postgrex.PgOutput do
 
   ## Examples
 
-    import Postgrex.PgOutput.Messages
-    <<lsn::64>> = Postgrex.PgOutput.Lsn.encode({0, 1})
+      import Postgrex.PgOutput.Messages
+      <<lsn::64>> = Postgrex.PgOutput.Lsn.encode({0, 1})
 
-    msg =
-      msg_standby_status_update(
-        wal_recv: lsn + 1,
-        wal_flush: lsn + 1,
-        wal_apply: lsn + 1,
-        system_clock: now(),
-        reply: 0
-      )
+      msg =
+        msg_standby_status_update(
+          wal_recv: lsn + 1,
+          wal_flush: lsn + 1,
+          wal_apply: lsn + 1,
+          system_clock: now(),
+          reply: 0
+        )
 
-    bin_msg = Postgrex.PgOutput.encode(msg)
+      bin_msg = Postgrex.PgOutput.encode(msg)
   """
   @spec encode(term()) :: binary()
   defdelegate encode(msg), to: __MODULE__.Messages
