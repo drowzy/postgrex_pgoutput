844df545d41eb032e40b7b1b3e6781aa596ffa5c
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2022-11-08 10:23:08 +0100

    prepare publish

--- mix.exs
+++ mix.exs
@@ -1,12 +1,19 @@
-defmodule Postgrex.Pgoutput.MixProject do
+defmodule Postgrex.PgOutput.MixProject do
   use Mix.Project
+
+  @version "0.1.0"
+  @source_url "https://github.com/drowzy/postgrex_pgoutput"
+  @description "Decode / encode Postgres replication messages"
 
   def project do
     [
       app: :postgrex_pgoutput,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
+      description: @description,
+      package: package(),
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -18,11 +25,36 @@ defmodule Postgrex.Pgoutput.MixProject do
     ]
   end
 
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "Postgrex.PgOutput",
+      source_url: @source_url,
+      source_ref: "v#{@version}"
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Simon Thörnqvist"],
+      licenses: ["MIT"],
+      files: ~w(
+        mix.exs
+        README.md
+        lib/postgrex
+        LICENSE
+        .formatter.exs
+      ),
+      links: %{"GitHub" => @source_url}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:postgrex, "~> 0.16.5", optional: true},
-      {:jason, "~> 1.2", optional: true}
+      {:jason, "~> 1.2", optional: true},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
