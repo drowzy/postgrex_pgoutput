0714835bc6010220ef3aef132d4200da6277214d
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-12-17 01:30:34 +0100

    add a test that uses values that would create negative values

--- test/postgrex/pg_output/lsn_test.exs
+++ test/postgrex/pg_output/lsn_test.exs
@@ -4,34 +4,41 @@ defmodule Postgrex.PgOutput.LsnTest do
 
   describe "encode/decode" do
     test "can decode lsn" do
-      assert {0, 685_397_688} == Lsn.decode(<<0, 0, 0, 0, 40, 218, 86, 184>>)
+      assert dbg({0, 685_397_688}) == dbg(Lsn.decode(<<0, 0, 0, 0, 40, 218, 86, 184>>))
     end
 
     test "can encode lsn" do
-      assert <<0, 0, 0, 0, 40, 218, 86, 184>> == Lsn.encode({0, 685_397_688})
+      assert dbg(<<0, 0, 0, 0, 40, 218, 86, 184>>) == dbg(Lsn.encode({0, 685_397_688}))
     end
 
     test "can decode string" do
-      assert {0, 0} = Lsn.decode_string("0/0")
+      assert dbg({0, 0}) == dbg(Lsn.decode_string("0/0"))
     end
 
     test "can decode string with offset" do
-      assert {0, 685_397_688} = Lsn.decode_string("0/28DA56B8")
+      assert dbg({0, 685_397_688}) == dbg(Lsn.decode_string("0/28DA56B8"))
     end
 
     test "can encode string" do
-      assert "0/0" = Lsn.encode_string({0, 0})
+      assert dbg("0/0") == dbg(Lsn.encode_string({0, 0}))
     end
 
     test "can encode string with offset" do
-      lsn = "0/28DA56B8"
+      lsn = dbg("0/28DA56B8")
 
       lsn2 =
         lsn
         |> Lsn.decode_string()
         |> Lsn.encode_string()
+        |> dbg()
 
       assert lsn == lsn2
+    end
+
+    @tag :focus
+    test "can handle values in the unsigned range" do
+      assert Lsn.encode_int64({0, 4_294_967_295}) == 4_294_967_295
+      assert Lsn.decode(Lsn.encode({0, 4_294_967_295})) == Lsn.decode_string("0/FFFFFFFF")
     end
   end
 end
