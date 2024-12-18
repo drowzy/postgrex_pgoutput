63c3eef38cc6bb9ce048110ec018a533cda1b8d9
Simon Thörnqvist <simon.thornqvist@erlang-solutions.com>
2024-12-18 16:07:15 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Postgrex.PgOutput.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.2.0"
   @source_url "https://github.com/drowzy/postgrex_pgoutput"
   @description "Decode / encode Postgres replication messages"
 
